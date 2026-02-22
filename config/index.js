d9d7ccd5e03453fc1fcdbf0008f542486123bbe4
AbialiDr <luckybelt79@gmail.com>
2026-02-22 02:58:17 +0000

    feat: Add Dockerization for the frontend application and update the S3 proxy configuration.

--- config/index.js
+++ config/index.js
@@ -3,8 +3,7 @@
 
 // export const PROXY = "http://localhost:8080";
 export const PROXY = "https://backend.wedfield.com";
-
-export const S3PROXY = "https://wedfield.s3.ap-south-1.amazonaws.com";
+export const S3PROXY = "https://wedfield-prod.s3.ap-south-1.amazonaws.com";
 //
 export const firebaseUrl =
   "https://wedfield-743f6-default-rtdb.asia-southeast1.firebasedatabase.app";
